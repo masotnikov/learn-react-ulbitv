b841e43dafeef7dc387bc2424f2fab2618b4b348
masotnikov <motrunich2011@gmail.com>
2023-12-16 00:43:06 +0500

    create increment/decrement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{likes}</h1>
+      <button onClick={() => setLikes(likes + 1)}>+</button>
+      <button onClick={() => setLikes(likes - 1)}>-</button>
+    </>
   );
 }
 
