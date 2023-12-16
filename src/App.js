078e6e48a2ea8f78085979869a725bfb531fd444
masotnikov <motrunich2011@gmail.com>
2023-12-16 22:04:11 +0500

    create controlled input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import {useState} from "react";
 
 function App() {
-  const [likes, setLikes] = useState(0)
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('ТЕКСТ в ИНПУТЕ');
+
   return (
     <>
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
       <button onClick={() => setLikes(likes + 1)}>+</button>
       <button onClick={() => setLikes(likes - 1)}>-</button>
     </>
