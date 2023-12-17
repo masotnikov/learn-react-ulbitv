b0aedcab72c3fe723c667c6ca3ddf72038dcd6cd
masotnikov <motrunich2011@gmail.com>
2023-12-17 12:10:19 +0500

    add component ClassCounter.jsx

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState('ТЕКСТ в ИНПУТЕ');
 
   return (
-    <>
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-      <button onClick={() => setLikes(likes + 1)}>+</button>
-      <button onClick={() => setLikes(likes - 1)}>-</button>
-    </>
+    <div className="App">
+      <Counter/>
+      <ClassCounter/>
+    </div>
   );
 }
 
