4f89e6f63e47137bff2471da49e2658e95db6dde
masotnikov <motrunich2011@gmail.com>
2023-12-17 13:52:48 +0500

    create PostItem.jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './styles/App.css'
+import PostItem from "./components/PostItem";
+import {useState} from "react";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'}
+  ]);
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. JavaScript</strong>
-          <div>
-            JavaScript - язык программирования
-          </div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-        </div>
-      </div>
+      <h1 style={{textAlign: 'center'}}>Список постов</h1>
+      {posts.map(item => (
+        <PostItem key={item.id} post={item}/>
+      ))}
     </div>
   );
 }
