00898c5f23d4ece5bf9c956a275c38865fcf700c
masotnikov <motrunich2011@gmail.com>
2023-12-21 14:02:36 +0500

    refactored the code in the component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './styles/App.css'
-import {useMemo, useState} from "react";
+import {useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,34 +13,26 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ]);
-
   const [filter, setFilter] = useState({sort: '', query: ''});
-
-  const sortedPosts = useMemo(() => {
-    console.log('ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТС');
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts
-  }, [posts, filter.sort]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
-
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   //создание поста
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 //удаление поста
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-    return (
+  return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <MyButton onClick={() => setModal(true)} style={{marginTop: 15}}>Создать пользователя</MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
       <hr style={{margin: '15px 0'}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
       <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов 1'}/>
