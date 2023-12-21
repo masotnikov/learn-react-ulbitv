bad2915b37d60250d6ee06e0116ebe5902bcca39
masotnikov <motrunich2011@gmail.com>
2023-12-21 14:01:24 +0500

    add features to the MyModal.jsx

--- src/components/UI/MyModal/MyModal.jsx
+++ src/components/UI/MyModal/MyModal.jsx
@@ -1,10 +1,16 @@
-import classes from '/MyModal.module.css'
+import classes from './MyModal.module.css'
 
-const MyModal = ({children}) => {
+const MyModal = ({children, visible, setVisible}) => {
+
+  const rootClasses = [classes.myModal];
+  if (visible) {
+    rootClasses.push(classes.active)
+  }
   return (
-    <div className={[classes.myModal, classes.active].join(' ')}>
-      <div className={classes.myModalContent}></div>
-      {children}
+    <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
+      <div className={classes.myModalContent} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
     </div>
   )
 }
