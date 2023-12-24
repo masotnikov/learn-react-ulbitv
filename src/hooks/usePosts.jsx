bdd20e54b9768537d0e892d4c91b7702e574c813
masotnikov <motrunich2011@gmail.com>
2023-12-24 23:25:36 +0500

    refactored the code in the component usePosts.jsx

--- src/hooks/usePosts.jsx
+++ src/hooks/usePosts.jsx
@@ -1,22 +1,18 @@
 import {useMemo} from "react";
 
 export const useSortedPosts = (posts, sort) => {
-  const sortedPosts = useMemo(() => {
-    console.log('ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТС');
+  return useMemo(() => {
     if (sort) {
       return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]));
     }
     return posts
-  }, [posts, sort]);
-
-  return sortedPosts
+  }, [posts, sort])
 }
 
 export const usePosts = (posts, sort, query) => {
 
   const sortedPosts = useSortedPosts(posts, sort)
-  const sortedAndSearchedPosts = useMemo(() => {
+  return useMemo(() => {
     return sortedPosts.filter(post => post.title.toLowerCase().includes(query.toLowerCase()))
   }, [query, sortedPosts])
-  return sortedAndSearchedPosts
 }
