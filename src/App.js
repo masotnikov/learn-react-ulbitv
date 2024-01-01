a3ce24f28d2b6ef6bc4d32fe12891c10302b7e27
masotnikov <motrunich2011@gmail.com>
2024-01-01 22:14:22 +0500

    refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,16 @@
 import './styles/App.css'
-import {useEffect, useState} from "react";
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import MyButton from "./components/UI/button/MyButton";
-import {usePosts} from "./hooks/usePosts";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/Loader/Loader";
-import {useFetching} from "./hooks/useFetching";
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./components/UI/Navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({sort: '', query: ''});
-  const [modal, setModal] = useState(false);
-  const [totalCount, setTotalCount] = useState(0);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fecthPosts, isPostsLoading, postError] = useFetching(async () => {
-    const response = await PostService.getAll();
-    setPosts(response.data);
-    console.log(response.headers['x-total-count'])
-    setTotalCount(response.headers['x-total-count'])
-  })
-
-  //создание поста
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
-
-  useEffect(() => {
-    fecthPosts();
-  }, []);
-
-
-
-//удаление поста
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
 
   return (
-    <div className="App">
-      <MyButton onClick={() => setModal(true)} style={{marginTop: 15}}>Создать пользователя</MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost}/>
-      </MyModal>
-      <hr style={{margin: '15px 0'}}/>
-      <PostFilter filter={filter} setFilter={setFilter}/>
-      {postError &&
-        <h1 style={{textAlign:'center'}}>Произошла ошибка...</h1>}
-      {isPostsLoading
-        ? <div style={{display: 'flex', justifyContent: 'center'}}><Loader/></div>
-        :
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов 1'}/>
-      }
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar/>
+      <AppRouter/>
+    </BrowserRouter>
+  )
 }
 
 export default App;
